abae41a165deff9435e9708c13b789dabb4e60a3
Georg Ringer <georg.ringer@gmail.com>
2023-05-03 00:08:50 +0200

    [TASK] Improve checks

--- Classes/Service/Validation.php
+++ Classes/Service/Validation.php
@@ -2,30 +2,33 @@
 
 namespace GeorgRinger\LoginLink\Service;
 
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
 
 class Validation
 {
 
-    public function isValid(string $table, int $recordId):bool
+    public function isValid(string $table, int $recordId): bool
     {
         if (!in_array($table, ['be_users', 'fe_users'], true)) {
             return false;
         }
 
         $user = $this->getBackendUser();
-        if ($user->isAdmin()) {
-            return true;
-        }
+        $row = BackendUtility::getRecord($table, $recordId);
+
+        // BE
         if ($table === 'be_users') {
-            return false;
+            if ($row['disable'] || $row['admin']) {
+                return false;
+            }
+            return $user->isAdmin();
         }
 
+        // FE
         // todo allow editors to login for fe_users
-
-        return true;
+        return $user->isAdmin();
     }
-
 
     protected function getBackendUser(): BackendUserAuthentication
     {
