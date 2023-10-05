d53c39a3ec9e0db58d9066e99af8001e82e614f1
Stig Nørgaard Færch <stigfaerch@gmail.com>
2023-10-05 15:47:59 +0200

    [FEATURE] Magic login link plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,7 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['BE_fetchUserIfNoSession'] = true;
 $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'] = true;
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['templateRootPaths'][1695727924] = 'EXT:login_link/Resources/Private/Templates/Email/';
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
     'login_link',
@@ -18,4 +19,13 @@ $GLOBALS['TYPO3_CONF_VARS']['SVCONF']['auth']['setup']['FE_fetchUserIfNoSession'
         'exec' => '',
         'className' => \GeorgRinger\LoginLink\Authentication\TokenAuthenticationService::class,
     ]
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'LoginLink',
+    'MagicLoginLinkForm',
+    // all actions
+    [\GeorgRinger\LoginLink\Controller\PluginController::class => 'showForm,sendMail'],
+    [\GeorgRinger\LoginLink\Controller\PluginController::class => 'showForm,sendMail'],
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
